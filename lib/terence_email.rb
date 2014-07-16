17f9ba7ab5374ac23c60c6dceac92463a928ec42
Peter Grunde <peter.a.grunde@gmail.com>
2014-07-16 09:15:52 -0600

    initial setup

---
+++ lib/terence_email.rb
@@ -0,0 +1,9 @@
+class TerenceEmail
+  def initialize
+
+  end
+
+  def beep
+    "eee"
+  end
+end
---
+++ spec/terence_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe TerenceEmail do
+  it "foo" do
+    expect(TerenceEmail.new).to eq("foo")
+  end
+end
